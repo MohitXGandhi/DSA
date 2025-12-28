ebc0e1807a63a472f1120ed5413442a978b8b5be
MohitXGandhi <143180864+MohitXGandhi@users.noreply.github.com>
2025-12-28 12:44:16 +0530

    factorial

--- p4.java
+++ p4.java
@@ -1,7 +1,21 @@
+import java.util.*;
 public class p4 {
+    public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the number");
+    System.out.println("Enter the number=");
     int n = sc.nextInt();
-    
+    int factorial = 1;
+
+    while(n>0){
+      
+
+        //factorial check
+        
+        factorial *= n ;
+        n--;
+
+    }
+    System.out.println(factorial);
+    }
 
 }
